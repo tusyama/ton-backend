190364f66a35e0d87c5874ae7f4e9759a47df005
Artem Repin <lisesx@gmail.com>
2024-06-14 23:25:22 +1000

    feature: check staking, bridge, and balance tasks

---
+++ src/consts/bridges.ts
@@ -0,0 +1,4 @@
+const BSCbridge = "Ef9NXAIQs12t2qIZ-sRZ26D977H65Ol6DQeXc5_gUNaUys5r";
+const ETHbridge = "Ef_dJMSh8riPi3BTUTtcxsWjG8RLKnLctNjAM4rw8NN-xWdr";
+
+export { BSCbridge, ETHbridge };
---
+++ src/consts/stakings.ts
@@ -0,0 +1,2 @@
+const mainStakingPool = "EQCkWxfyhAkim3g2DjKQQg8T5P4g-Q1-K_jErGcDJZ4i-vqR";
+export { mainStakingPool };
